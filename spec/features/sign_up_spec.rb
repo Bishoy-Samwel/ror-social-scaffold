d85ca3174f6d026aaaf005f288150cceebe0a86f
bishoy-samwel <bishoy-samwuel@gmail.com>
2021-05-27 15:24:54 +0200

    Login and sign up test

---
+++ spec/features/sign_up_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+RSpec.describe 'Create new User proccess', type: :system do
+
+  it 'Sign up' do
+    visit root_path
+    click_link_or_button 'Sign up'
+    fill_in 'Name', with: 'User1'
+    fill_in 'Email', with: 'user1@gmail.com'
+    fill_in 'Password', with: 'password'
+    fill_in 'Password confirmation', with: 'password'
+    click_button 'Sign up'
+    expect(page).to have_text('Welcome! You have signed up successfully.')
+  end
+end
