d11e357af7e0cee94554f57337f3fbd54c09ebd7
Luqman Musah <musahluqman@gmail.com>
2021-12-15 09:53:14 +0000

    Add show comments to routes

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,6 @@ Rails.application.routes.draw do
     resources :comments, only: [:create]
     resources :likes, only: [:create, :destroy]
   end
-
+get "comments/:id", to: "comments#show"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
