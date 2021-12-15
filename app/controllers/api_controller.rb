d793b947a993ca1a5601c53cbfee9761bf1f86fc
Bishoy <bishoy.samwuel@gmail.com>
2021-12-15 17:04:45 +0200

    add create_comment method

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,38 +1,26 @@
-class ApiController < ApplicationController
-    before_action :authenticate_user!
-    protect_from_forgery with: :null_session
+class ApiController < ActionController::API
+    # protect_from_forgery with: :null_session
   
-    def index
-      @post = Post.new
+    def posts_index
       render json: timeline_posts
     end
-    def create
-      @post = current_user.posts.new(post_params)
-  
-      if @post.save
-        redirect_to posts_path, notice: 'Post was successfully created.'
+
+    def show_comments
+      @post_comments = Post.find(params[:post_id]).comments
+      render json: @post_comments
+    end
+
+    def create_comment
+      @comment = Comment.create(comment_params)
+      render json: @comment
+      if @comment.save
+        render json: @comment
+        # redirect_to posts_path, notice: 'Comment was successfully created.'
       else
-        timeline_posts
-        render :index, alert: 'Post was not created.'
+        puts "not saving"
+        # redirect_to posts_path, alert: @comment.errors.full_messages.join('. ').to_s
       end
     end
-    def show_comment
-        @post_comments = Post.find(params[:post_id]).comments
-        render json: @post_comments
-      end
-
-      def create_comment
-        @comment = Comment.new(comment_params)
-        @comment.post_id = params[:post_id]
-        @comment.user = User.find(params[:user_id])
-        render json: @comment
-    
-        if @comment.save
-          redirect_to posts_path, notice: 'Comment was successfully created.'
-        else
-          redirect_to posts_path, alert: @comment.errors.full_messages.join('. ').to_s
-        end
-      end
 
     private
   
@@ -43,9 +31,9 @@ class ApiController < ApplicationController
       end
       @timeline_posts
     end
-  
-    def post_params
-      params.require(:post).permit(:content)
+
+    def comment_params
+      params.permit(:content, :user_id, :post_id)
     end
   end
   
