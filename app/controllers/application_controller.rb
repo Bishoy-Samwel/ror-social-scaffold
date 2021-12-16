97114c584aeba741b383b38f4437993b364cca2b
Bishoy <bishoy.samwuel@gmail.com>
2021-12-16 16:06:17 +0200

    Add authorize_request

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,23 @@
 class ApplicationController < ActionController::Base
+
   protect_from_forgery with: :exception
   protect_from_forgery unless: -> { request.format.json? } 
   before_action :configure_permitted_parameters, if: :devise_controller?
+  # include Response
+  # include ExceptionHandler
+
+  # called before every action on controllers
+  before_action :authorize_request
+  attr_reader :current_user
+
+  private
+
+  # Check for valid request token and return user
+  def authorize_request
+    @current_user = (AuthorizeApiRequest.new(request.headers).call)[:user]
+  end
+
+
 
   protected
 
