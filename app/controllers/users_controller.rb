09474182589bbd554e5d88d712b333f6a9e4b7d9
Bishoy <bishoy.samwuel@gmail.com>
2021-12-16 16:07:05 +0200

    Add Create to UserController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,26 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
-
+  skip_before_action :authorize_request, only: :create
   def index
     @users = User.all
     @pending_friends = current_user.pending_requests
     @requesting_friends = current_user.friend_requests
+  end
+
+  def create
+    user = User.create!(user_params)
+    auth_token = AuthenticateUser.new(user.email, user.password).call
+    response = { message: Message.account_created, auth_token: auth_token }
+    json_response(response, :created)
+  end
+
+  def user_params
+    params.permit(
+      :name,
+      :email,
+      :password,
+      :password_confirmation
+    )
   end
 
   def show
