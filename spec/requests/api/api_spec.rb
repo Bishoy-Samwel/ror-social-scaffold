32d59437e38eecea105c36a68f7d7f79080e86fd
Bishoy <bishoy.samwuel@gmail.com>
2021-12-17 17:32:50 +0200

    Fix rubocop issues

--- spec/requests/api/api_spec.rb
+++ spec/requests/api/api_spec.rb
@@ -30,32 +30,32 @@ RSpec.describe 'api/api', type: :request do
         run_test!
       end
     end
+  end
 
-    path 'comments/add/{user_id}/{post_id}/{content}' do
-      post 'Create a Comment' do
-        tags 'Comments'
-        consumes 'application/json', 'application/xml'
-        parameter name: :post, in: :body, schema: {
-          type: :object,
-          properties: {
-            content: { type: :string },
-            post_id: { type: :integer },
-            user_id: { type: :integer },
-            created_at: { type: :date },
-            update_at: { type: :date }
-          },
-          required: ['content']
-        }
+  path 'comments/add/{user_id}/{post_id}/{content}' do
+    post 'Create a Comment' do
+      tags 'Comments'
+      consumes 'application/json', 'application/xml'
+      parameter name: :post, in: :body, schema: {
+        type: :object,
+        properties: {
+          content: { type: :string },
+          post_id: { type: :integer },
+          user_id: { type: :integer },
+          created_at: { type: :date },
+          update_at: { type: :date }
+        },
+        required: ['content']
+      }
 
-        response '201', 'post listed' do
-          let(:comment) { { content: 'new content' } }
-          run_test!
-        end
+      response '201', 'post listed' do
+        let(:comment) { { content: 'new content' } }
+        run_test!
+      end
 
-        response '422', 'invalid request' do
-          let(:comment) { { name: 'old content' } }
-          run_test!
-        end
+      response '422', 'invalid request' do
+        let(:comment) { { name: 'old content' } }
+        run_test!
       end
     end
   end
