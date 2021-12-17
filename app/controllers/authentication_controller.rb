45bf09b4f6f164e2dc6f510d91b5dd8ac4b23561
Bishoy <bishoy.samwuel@gmail.com>
2021-12-17 10:41:10 +0200

    Update the authenticity

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -1,8 +1,6 @@
 class AuthenticationController < ApplicationController
-  # return auth token once user is authenticated
-  skip_before_action :authorize_request, only: :authenticate
-  protect_from_forgery unless: -> { request.format.json? } 
-
+  skip_before_action :authorize_request, only: :authentication
+  protect_from_forgery with: :null_session, only: [:authentication] 
   def authenticate
     auth_token =
       AuthenticateUser.new(auth_params[:email], auth_params[:password]).call
