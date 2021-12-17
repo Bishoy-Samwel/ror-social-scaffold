c7b78ab294c8bb3c9b33ac19ff453fa84121435c
Luqman Musah <musahluqman@gmail.com>
2021-12-17 15:19:21 +0000

    Add test for creating a comment

--- spec/requests/api/api_spec.rb
+++ spec/requests/api/api_spec.rb
@@ -3,79 +3,58 @@ require 'swagger_helper'
 RSpec.describe 'api/api', type: :request do
   path '/comments/{post_id}' do
 
-    get 'Retrieves a blog' do
-      tags 'Blogs', 'Another Tag'
+    get 'Retrieves a Comment' do
+      tags 'Comments', 'Another Tag'
       produces 'application/json', 'application/xml'
-      parameter name: :id, in: :path, type: :string
+      parameter name: :id, type: :string
 
-      response '200', 'blog found' do
+      response '200', 'Comment found' do
         schema type: :object,
           properties: {
-            id: { type: :integer },
-            title: { type: :string },
-            content: { type: :string }
+            content: { type: :string },
+            post_id: {type: :integer},
+            user_id: {type: :integer},
+            created_at: {type: :date},
+            update_at: {type: :date}
           },
-          required: [ 'id', 'title', 'content' ]
+          required: [ 'id', 'user_id', 'post_id', 'content', 'created_at', 'updated_at' ]
 
-        let(:id) { Blog.create(title: 'foo', content: 'bar').id }
+        let(:id) { Comment.create(id: 2, user_id: 1, post_id: 1, content: "sdsds", created_at: "2021-12-15 12:25:17.388417000", updated_at: "2021-12-15 12:25:17.388417000").id }
         run_test!
       end
 
-      response '404', 'blog not found' do
+      response '404', 'Comment not found' do
         let(:id) { 'invalid' }
         run_test!
       end
-
-      response '406', 'unsupported accept header' do
-        let(:'Accept') { 'application/foo' }
-        run_test!
-      end
     end
-  end
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-describe 'Posts API' do
-  path '/getPosts' do
-    post 'Show all POsts' do
-      tags 'Posts'
+  path 'comments/add/{user_id}/{post_id}/{content}' do
+    post 'Create a Comment' do
+      tags 'Comments'
       consumes 'application/json', 'application/xml'
       parameter name: :post, in: :body, schema: {
         type: :object,
         properties: {
           content: { type: :string },
-          id: {type: number},
-          user_id: {type: number},
-          created_at: {type: date},
-          update_at: {type: date}
+          post_id: {type: :integer},
+          user_id: {type: :integer},
+          created_at: {type: :date},
+          update_at: {type: :date}
         },
         required: [ 'content' ]
       }
 
       response '201', 'post listed' do
-        let(:post) { Post.all }
+        let(:comment) { { content: 'new content' } }
         run_test!
       end
 
       response '422', 'invalid request' do
-        let(:post) { { 'invalid' }
+        let(:comment) { { name: 'old content' } }
         run_test!
       end
     end
   end
 end
-
-
+end
